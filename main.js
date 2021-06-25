a1d5ba317f8988195f0f397880fe71898d63b12d
Aaradhana Bharathwaj <73657462+aaradhanabharathwaj@users.noreply.github.com>
2021-06-25 17:54:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function next_page2(){
+    window.location="services.html";
+}
+function next_page3(){
+    window.location="amazon_delivery.html";
+    
+}
+function next_page4(){
+    window.location="primevideo.html";
+}
+function back1(){
+    window.location="services1.html";
+}
+function back2(){
+    window.location="amazon_delivery.html";
+}
+function next5(){
+    window.location="music.html"
+}
+function back3(){
+    window.location="founder.html";
+}
+function next6(){
+    window.location="thankyou.html";
+}
+function back5(){
+    window.location="primevideo.html"
+}
+function founder(){
+    window.location="founder.html";
+}
